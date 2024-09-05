4f644ba9715dcc11f2979146f655cbc7d9a4bf85
Elisafa <elisafajunior@hotmail.com>
2024-09-05 15:10:31 -0300

    Criando os componentes 06 e 07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { Componente02Component } from "./components/componente02/componente02.co
 import { Componente03Component } from "./components/componente03/componente03.component";
 import { Componente04Component } from "./components/componente04/componente04.component";
 import { Componente05Component } from "./components/componente05/componente05.component";
+import { Componente06Component } from "./components/componente06/componente06.component";
+import { Componente07Component } from "./components/componente07/componente07.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component],
+  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component, Componente07Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/componente05/componente05.component.ts
+++ src/app/components/componente05/componente05.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-componente05',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './componente05.component.html',
   styleUrl: './componente05.component.css'
 })
 export class Componente05Component {
+  // Criando um vetor de nomes
+  nomes:string[] = ["Elisafá", "Elioenay", "Pedro", "Rebeca"];
 
 }
