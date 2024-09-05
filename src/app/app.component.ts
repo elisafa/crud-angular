a10f71c3f41f04767c06c9f7c91fc872408962bd
Elisafa <elisafajunior@hotmail.com>
2024-09-05 17:15:40 -0300

    Criado o projeto e a pasta de modelo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ import { Componente07Component } from "./components/componente07/componente07.co
 import { Componente08Component } from "./components/componente08/componente08.component";
 import { Componente09Component } from "./components/componente09/componente09.component";
 import { Componente10Component } from "./components/componente10/componente10.component";
+import { Componente11Component } from "./components/componente11/componente11.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component, Componente07Component, Componente08Component, Componente09Component, Componente10Component],
+  imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component, Componente07Component, Componente08Component, Componente09Component, Componente10Component, Componente11Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
