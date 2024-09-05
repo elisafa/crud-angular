5b98f688e9c81d053e0d1e3f79d6f2fb7605402b
Elisafa <elisafajunior@hotmail.com>
2024-09-05 18:55:56 -0300

    Implementando o crud

--- src/app/components/componente11/componente11.component.ts
+++ src/app/components/componente11/componente11.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../../modelo/Pessoa';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-componente11',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './componente11.component.html',
   styleUrl: './componente11.component.css'
 })
@@ -16,5 +19,41 @@ formulario = new FormGroup({
   idade   : new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]), 
   cidade  : new FormControl('', [Validators.required, Validators.minLength(3)])
 });
+
+// Visibilidade dos botoes
+btnCadastrar:boolean = true;
+
+// Vetor
+vetor:Pessoa[] = [];
+
+// Armazenar o indicie da pessoa selecionada
+indice:number = -1;
+
+// Funcao de cadastro
+cadastrar(){
+
+  //cadastro no vetor
+  this.vetor.push(this.formulario.value as Pessoa);
+
+  // limpeza dos inputs
+  this.formulario.reset();
+
+  //visualizaçao via console
+  // console.table(this.vetor)
+}
+
+selecionar(indice:number){
+  //Atribuir o indice da pessoa
+  this.indice = indice;
+
+  //Atribuir os dado
+  this.formulario.setValue({
+    nome : this.vetor[indice].nome,
+    idade : this.vetor[indice].idade,
+    cidade : this.vetor[indice].cidade
+  });
+
+  this.btnCadastrar = false;
+}
 
 }
